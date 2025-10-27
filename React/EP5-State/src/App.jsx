2a11828a2216c6a981ea0db0e88fdf6869dda1be
CodeWithArjunan <duraiarjun94@gmail.com>
2025-10-27 17:45:19 +0530

    EP5-States

--- React/EP5-State/src/App.jsx
+++ React/EP5-State/src/App.jsx
@@ -4,30 +4,60 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let divbox={
+    backgroundColor: 'aqua',
+    color: "black",
+    height: "fit-content",
+    width: "fit-content",
+    padding: "10px",
+    borderRadius: "8%",
+    textAlign: "center",
+}
+   let button = {
+    backgroundColor: "red",
+    color: "white",
+    cursor: "pointer",
+    padding: "8px",
+    borderRadius: "20%",
+    margin: "1px",
+  };
+
+
+  let [count,setCount]=useState(1); 
+//const  [State,setState]=useState(initialVal);
+  
+  
+  let [price,setprice]=useState(priceVal); //also use function as a initialval
+  function priceVal(){
+    return 100;
+  }
+
+  function inc(){
+    //setCount(count+1); =1
+    //setCount(count+1); =1
+    //setCount(count+1); =1 -->Same Value not changed , So we can use the parameter to pass the value and update
+    setCount( (preVal) => preVal+1);
+    setCount( (preVal) => preVal+1); // don't use the {preVal+1} , inc 2 val  
+    console.log("Inc");
+    count >20? alert("you reach over 10") : "";
+  }
+
+  function dec(){
+    // setCount(count-1);
+    setCount( (preVal) => preVal-1);
+    setCount( (preVal) => preVal-1);
+    console.log("Dec");
+    count <-20? alert("you are too low") : "";
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={divbox} >
+        <p>Click the button to unlock the price {count}</p>
+        <button onClick={inc} style={button}>Inc</button>
+        <button onClick={dec} style={button}>Dec</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
